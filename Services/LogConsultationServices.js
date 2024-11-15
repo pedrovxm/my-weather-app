287952757a957b9046cc66d4a060fc36ae172a45
Pedro Moreira <pedrovxm@gmail.com>
2024-11-15 16:10:32 -0300

    removing log

--- Services/LogConsultationServices.js
+++
@@ -1,7 +0,0 @@
-const LogConsultation = require('../Models/LogConsultationModel.js')
-
-
-
-async function createLog(userData){
-
-}
--- Services/WeatherService.js
+++ Services/WeatherService.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const { createClient } = require('redis');
 
-// Configuração do Redis
+
 const client = createClient();
 
 client.connect()
   .then(() => console.log('Connecting on Redis'))
   .catch((err) => console.error('Error connecting on Redis:', err));
 
-// Função para obter dados do cache
+
 async function getAsync(key) {
   try {
     const data = await client.get(key);
@@ -19,7 +19,7 @@ async function getAsync(key) {
   }
 }
 
-// Função para salvar dados no cache
+
 async function setAsync(key, value, ttl) {
   try {
     await client.set(key, JSON.stringify(value), { EX: ttl }); // TTL em segundos
@@ -29,7 +29,7 @@ async function setAsync(key, value, ttl) {
   }
 }
 
-// Função para substituir espaços por caracteres personalizados
+
 function replaceSpaces(input, replacementChar) {
   if (typeof input !== 'string' || typeof replacementChar !== 'string') {
     throw new Error('Parameters should be strings');
@@ -39,19 +39,19 @@ function replaceSpaces(input, replacementChar) {
 
 async function getWeather(city) {
   try {
-    const startTime = Date.now(); // Marca o início da operação
+    const startTime = Date.now(); 
 
     // Tenta obter do cache
     const cachedWeather = await getAsync(city);
 
     if (cachedWeather) {
-      const endTime = Date.now(); // Marca o fim da operação
+      const endTime = Date.now(); 
       console.log(`Time taken (cache): ${endTime - startTime}ms`);
       console.log('Returning cached data');
       return cachedWeather;
     }
 
-    // Caso não esteja no cache, consulta a API
+    // Case not in cache
     console.log('Consulting API');
 
     const cityQuery = replaceSpaces(city, '%20');
@@ -61,10 +61,11 @@ async function getWeather(city) {
 
     const weatherData = response.data;
 
-    // Salva no cache por 1 hora (3600 segundos)
+  
     await setAsync(city, weatherData, 3600);
 
-    const endTime = Date.now(); // Marca o fim da operação
+
+    const endTime = Date.now();
     console.log(`Time taken (API): ${endTime - startTime}ms`);
 
     return weatherData;
